5141d0d0d79bffa1060ea060769f4c06b9782854
Minusten <chancyfancy23@gmail.com>
2025-01-18 01:33:06 +0530

    feat: Initial commit

---
+++ src/main/kotlin/com/github/itsthecatdev/iCore/ICore.kt
@@ -0,0 +1,12 @@
+package com.github.itsthecatdev.iCore
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ICore : JavaPlugin() {
+
+    override fun onEnable() {
+    }
+
+    override fun onDisable() {
+    }
+}
